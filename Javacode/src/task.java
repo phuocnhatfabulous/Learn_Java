63f91cfc1f462f2e5cac6b5047c00e48a3f431a5
DoanPhuocNhat <doanphuocnhat205@gmail.com>
2021-03-07 17:31:22 +0700

    Input_Output

--- Javacode/src/task.java
+++ Javacode/src/task.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class task {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in );
 		System.out.println("hiiiii, input your name:  ");
 		String name = input.nextLine(); 
 		System.out.println("My name is: " + name );
+		System.out.println("I'm junior year!!! ");
 	}
 
 }
