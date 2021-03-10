a5f3f879bb7825f9ae83f9ffeb489173209e7573
DoanPhuocNhat <doanphuocnhat205@gmail.com>
2021-03-10 19:52:17 +0700

    GUI

---
+++ GUI/src/Interface/basic.java
@@ -0,0 +1,20 @@
+package Interface;
+
+import javax.swing.JOptionPane;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = JOptionPane.showInputDialog("Enter your name: ");
+		JOptionPane.showMessageDialog(null, "Hello" + name );
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
+		JOptionPane.showMessageDialog(null, "You're: " + age + "years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height: " ));
+		JOptionPane.showMessageDialog(null, "You're: " + height);
+
+	}
+
+}
